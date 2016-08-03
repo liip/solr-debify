1fad4408ef2aaf6dccbd41747429db3cb80ebd8e
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2016-08-03 16:24:30 +0200

    Make all that in one single script

---
+++ debian/scripts/gitlabci-runner.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+finish() {
+    date
+    vagrant halt
+    date
+}
+
+set -e
+trap finish EXIT INT
+
+export VIRTUALIZATION_PARAMETERS_FILE=virtualization/parameters.yml
+
+date
+vagrant up  --provider lxc
+date
+vagrant provision
+date
+
+echo "Get the original source"
+vagrant ssh -c "./debian/rules get-orig-source"
+
+echo "Build the package"
+vagrant ssh -c "dpkg-buildpackage -b"
