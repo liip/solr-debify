c0b78740ad0e77e85859e45323129151b20384de
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2016-08-15 13:59:39 +0200

    Test binary package installation

--- gitlabci-runner.sh
+++ gitlabci-runner.sh
@@ -27,3 +27,9 @@ vagrant ssh -c "cd solr-build; QUILT_PATCHES=debian/patches quilt push -af" || :
 
 echo "* Build the package"
 vagrant ssh -c "cd solr-build; dpkg-buildpackage -b -uc"
+
+echo "* Purge previous package traces"
+vagrant ssh -c "sudo apt-get -y purge solr; sudo rm -Rf /var/lib/solr /var/log/solr"
+
+echo "* Install the package in the vagrant box"
+vagrant ssh -c "sudo dpkg -i solr_*_all.deb; sudo apt-get install -f -y"
